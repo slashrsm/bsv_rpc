944ebbf0135fceca3cc9af306ef7ebaeedcd332c
Janez Urevc <janez@janezurevc.name>
2019-08-13 14:19:19 +0200

    Update Travis config.

--- mix.exs
+++ mix.exs
@@ -21,7 +21,12 @@ defmodule BsvRpc.MixProject do
         ignore_warnings: ".dialyzer_ignore.exs"
       ],
       test_coverage: [tool: ExCoveralls],
-      preferred_cli_env: [coveralls: :test, "coveralls.detail": :test, "coveralls.post": :test, "coveralls.html": :test]
+      preferred_cli_env: [
+        coveralls: :test,
+        "coveralls.detail": :test,
+        "coveralls.post": :test,
+        "coveralls.html": :test
+      ]
     ]
   end
 
